43dc32073cbf8f0e308a45823b3a87bffc352058
Tadjaur <tadjaur@tas-groups.com>
2025-05-20 02:53:02 +0100

    add empty path generation

---
+++ lib/features/bible_reader/view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BibleReaderView extends StatelessWidget {
+  const BibleReaderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/intro/view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntroView extends StatelessWidget {
+  const IntroView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
