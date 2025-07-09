a0fb065f7a3164806c7c985e98d66009e79b50d5
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-07-09 22:10:51 +0200

    #8: Update unit test.

--- test/widgets/screenshots/widgets/language_select_golden_test.dart
+++ test/widgets/screenshots/widgets/language_select_golden_test.dart
@@ -5,20 +5,26 @@ import '../../../goldens.dart';
 import '../../language_select_test.dart' as language_select_test;
 
 void main() {
-  group('Goldens', () {
-    setUpAll(() async {
-      await language_select_test.setupAll(skipDb: true); // <-- skip DB
-    });
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-    setUp(() async {
-      await language_select_test.setupEach(skipDb: true); // <-- skip DB
-    });
-    testGoldens('LanguageView', (tester) async {
+  setUpAll(() async {
+    await language_select_test.setupAll(skipDb: true); // <-- skip DB
+  });
+
+  setUp(() async {
+    await language_select_test.setupEach(skipDb: true); // <-- skip DB
+  });
+
+  group('LanguageView', () {
+    testGoldens('display properly', (tester) async {
       await multiScreenMultiLocaleGolden(
         tester,
         language_select_test.languageWidgetWrapper(),
         'language_view',
       );
+    });
+    testWidgets('save properly', (WidgetTester tester) async {
+      // ...test code...
     });
   });
 }
