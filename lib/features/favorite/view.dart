e0965766619e43408ec96cd784154e118b577f64
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-08-10 23:28:58 +0200

    Implement favorite verses display in FavoriteView with delete functionality

--- lib/features/favorite/view.dart
+++ lib/features/favorite/view.dart
@@ -1,10 +1,53 @@
+import 'package:biblic_calendar/services/bible_api_service.dart';
+import 'package:biblic_calendar/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FavoriteView extends StatelessWidget {
-  const FavoriteView({super.key});
+  const FavoriteView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final api = Get.find<BibleApiService>();
+    return Obx(() {
+      final favs = api.favoriteVerses;
+      if (favs.isEmpty) {
+        return Center(
+          child: Text('No favorites yet.', style: Styles.i.tsHeader1),
+        );
+      }
+      return ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: favs.length,
+        itemBuilder: (context, idx) {
+          final fav = favs[idx];
+          return Card(
+            elevation: 8,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: ListTile(
+              title: Text(fav['text'] ?? '', style: Styles.i.tsBody1),
+              subtitle: Text(
+                'Book: ${fav['bookId']} | Chapter: ${fav['chapter']} | Verse: ${fav['verse']}',
+                style: Styles.i.tsBody,
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete, color: Colors.red),
+                onPressed: () async {
+                  await api.removeFavorite(
+                    fav['versionId'],
+                    fav['bookId'],
+                    fav['chapter'],
+                    fav['verse'],
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      );
+    });
   }
 }
