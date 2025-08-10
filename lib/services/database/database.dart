5c8350096f54e9ffaebaf7efb216ce5398fd0f40
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2025-08-10 23:28:36 +0200

    Refactor database service to implement IDatabase interface for improved dependency injection and testing

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -1,21 +1,25 @@
 import 'package:biblic_calendar/entities/settings.dart';
 import 'package:biblic_calendar/objectbox.g.dart';
 import 'package:flutter/foundation.dart';
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
-/// Service for handling application database.
-class Database extends GetxService {
-  /// The ObjectBox store.
+/// Abstract database service for dependency injection and testing.
+abstract class IDatabase {
+  Box<Settings> get settings;
+  Future<void> close();
+}
+
+/// Concrete implementation using ObjectBox.
+class Database extends GetxService implements IDatabase {
   final Store _store;
 
-  /// Constructs a new database.
   Database(this._store);
 
   static final key = const Key("database");
 
-  /// Returns the settings box.
+  @override
   Box<Settings> get settings => _store.box<Settings>();
 
   /// Create an instance of ObjectBox to use throughout the app.
@@ -23,10 +27,13 @@ class Database extends GetxService {
     if (isInMemory) {
       return Database(await openStore(directory: "memory:test-db"));
     }
-
     final docsDir = await getApplicationDocumentsDirectory();
     final store = await openStore(directory: p.join(docsDir.path, "bible"));
-
     return Database(store);
+  }
+
+  @override
+  Future<void> close() async {
+    _store.close();
   }
 }
